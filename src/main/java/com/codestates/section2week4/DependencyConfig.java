873f86479a938cc3cb66a6b89ba8e9d4160cf335
Lee dong yeol <dlehdduf09020@naver.com>
2023-04-06 01:12:33 +0900

    DI done.

--- src/main/java/com/codestates/section2week4/DependencyConfig.java
+++ src/main/java/com/codestates/section2week4/DependencyConfig.java
@@ -5,9 +5,11 @@ import com.codestates.section2week4.coffee.CoffeeService;
 import com.codestates.section2week4.member.MemberRepository;
 import com.codestates.section2week4.member.MemberService;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
+@ComponentScan
 public class DependencyConfig {
     @Bean // 빈을 통해 스프링 컨테이너에 등록
     public MemberService memberService() {
